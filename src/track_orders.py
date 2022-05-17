fa2dd9585fa273a33f944a84c0609eb6a6057861
Bruno Pinho <brunopinho@outlook.com>
2022-05-17 20:20:30 -0300

    feat: faz req. 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,66 @@
+from typing import Counter
+
+
 class TrackOrders:
+    def __init__(self) -> None:
+        self.orders = []
+
     # aqui deve expor a quantidade de estoque
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append([customer, order, day])
+        print(self.orders)
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        customer_orders = []
+
+        for order in self.orders:
+            if order[0] == customer:
+                customer_orders.append(order[1])
+
+        most_requested_food = Counter(customer_orders).most_common()[0][0]
+        return most_requested_food
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        food_options = set()
+        customer_orders = set()
+
+        for order in self.orders:
+            food_options.add(order[1])
+
+            if order[0] == customer:
+                customer_orders.add(order[1])
+
+        return food_options ^ customer_orders
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        open_days = set()
+        visited_days_per_customer = set()
+
+        for order in self.orders:
+            open_days.add(order[2])
+
+            if order[0] == customer:
+                visited_days_per_customer.add(order[2])
+
+        return open_days ^ visited_days_per_customer
 
     def get_busiest_day(self):
-        pass
+        visited_days = []
+
+        for order in self.orders:
+            visited_days.append(order[2])
+
+        most_visited_day = Counter(visited_days).most_common()[0][0]
+        return most_visited_day
 
     def get_least_busy_day(self):
-        pass
+        visited_days = []
+
+        for order in self.orders:
+            visited_days.append(order[2])
+
+        most_visited_day = Counter(visited_days).most_common()[-1][0]
+        return most_visited_day
