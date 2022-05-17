724a5835df0ed8cc0527a21e076bd994596a001b
Bruno Pinho <brunopinho@outlook.com>
2022-05-17 17:00:38 -0300

    fix: reduz complexidade req. 01

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -23,73 +23,72 @@ def get_joao_unrequested_food_and_unvisited_days(options, orders):
     return set(options) ^ set(orders)
 
 
+def get_data(orders):
+    foods_options = set()
+    open_days = set()
+
+    clients_orders = {}
+    clients_frequency_days = {}
+
+    for order in orders:
+        foods_options.add(order["pedido"])
+        open_days.add(order["dia"])
+
+        if (
+            order["cliente"] in clients_orders
+            and order["cliente"] in clients_frequency_days
+        ):
+            clients_orders[order["cliente"]].append(order["pedido"])
+            clients_frequency_days[order["cliente"]].append(order["dia"])
+        else:
+            clients_orders[order["cliente"]] = [order["pedido"]]
+            clients_frequency_days[order["cliente"]] = [order["dia"]]
+
+    maria_most_requested_orders = get_maria_most_requested_food(
+        clients_orders["maria"]
+    )
+
+    arnaldo_requested_times = get_requested_hamburguers_number(
+        clients_orders["arnaldo"]
+    )
+
+    joao_unrequested_foods = get_joao_unrequested_food_and_unvisited_days(
+        foods_options, clients_orders["joao"]
+    )
+
+    joao_unvisited_days = get_joao_unrequested_food_and_unvisited_days(
+        open_days, clients_frequency_days["joao"]
+    )
+
+    lines = [
+        maria_most_requested_orders,
+        arnaldo_requested_times,
+        joao_unrequested_foods,
+        joao_unvisited_days,
+    ]
+
+    return lines
+
+
 # req. 1
 def analyze_log(path_to_file):
     try:
         field_names = ["cliente", "pedido", "dia"]
-        maria_most_requested_orders = ""
-        arnaldo_requested_times = 0
-        joao_unrequested_foods = {}
-        joao_unvisited_days = {}
+        lines_to_write = []
 
         with open(path_to_file) as file:
             orders = csv.DictReader(file, fieldnames=field_names)
-
-            foods_options = set()
-            open_days = set()
-
-            clients_orders = {}
-            clients_frequency_days = {}
-
-            for order in orders:
-                foods_options.add(order["pedido"])
-                open_days.add(order["dia"])
-
-                if (
-                    order["cliente"] in clients_orders
-                    and order["cliente"] in clients_frequency_days
-                ):
-                    clients_orders[order["cliente"]].append(order["pedido"])
-                    clients_frequency_days[order["cliente"]].append(
-                        order["dia"]
-                    )
-                else:
-                    clients_orders[order["cliente"]] = [order["pedido"]]
-                    clients_frequency_days[order["cliente"]] = [order["dia"]]
-
-            maria_most_requested_orders = get_maria_most_requested_food(
-                clients_orders["maria"]
-            )
-
-            arnaldo_requested_times = get_requested_hamburguers_number(
-                clients_orders["arnaldo"]
-            )
-
-            joao_unrequested_foods = (
-                get_joao_unrequested_food_and_unvisited_days(
-                    foods_options, clients_orders["joao"]
-                )
-            )
-
-            joao_unvisited_days = get_joao_unrequested_food_and_unvisited_days(
-                open_days, clients_frequency_days["joao"]
-            )
-
-        lines = [
-            maria_most_requested_orders,
-            arnaldo_requested_times,
-            joao_unrequested_foods,
-            joao_unvisited_days,
-        ]
+            lines_to_write = get_data(orders)
 
         with open("data/mkt_campaign.txt", "+w") as file:
             """
             com base:
             https://www.pythontutorial.net/python-basics/python-write-text-file/
             """
-            for line in lines:
+            for line in lines_to_write:
                 file.write(str(line))
                 file.write("\n")
+
     except FileNotFoundError:
         if path_to_file.endswith(".csv"):
             raise FileNotFoundError(f"Arquivo inexistente:' {path_to_file}'")
@@ -98,4 +97,4 @@ def analyze_log(path_to_file):
 
 
 if __name__ == "__main__":
-    print(analyze_log("data/orders_1.txt"))
+    print(analyze_log("data/orders_1.csv"))
