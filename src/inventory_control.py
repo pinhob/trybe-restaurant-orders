f293826a776461b601f839271cafba9bc8e55d3d
Bruno Pinho <brunopinho@outlook.com>
2022-05-18 17:39:38 -0300

    feat: faz req. 4

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -17,9 +17,26 @@ class InventoryControl:
 
     def __init__(self):
         self.orders = []
+        self.inventory = {
+            "pao": 50,
+            "carne": 50,
+            "queijo": 100,
+            "molho": 50,
+            "presunto": 50,
+            "massa": 50,
+            "frango": 50,
+        }
 
     def add_new_order(self, customer, order, day):
-        self.orders.append([customer, order, day])
+        orders_options = self.get_available_dishes()
+
+        for ingredient in self.INGREDIENTS[order]:
+            self.inventory[ingredient] -= 1
+
+        if order in orders_options:
+            return self.orders.append([customer, order, day])
+
+        return False
 
     def get_quantities_to_buy(self):
         order_ingredients = []
@@ -41,3 +58,22 @@ class InventoryControl:
             total_ingredients[ingredient] += 1
 
         return total_ingredients
+
+    def get_available_dishes(self):
+        avaliable_dishes = set()
+
+        """
+        com consulta na solução do colega Matteus Fernandes,
+        para entender onde estava meu erro:
+        https://github.com/tryber/sd-013-c-restaurant-orders/pull/111
+        """
+        for dish in self.INGREDIENTS:
+            has_ingredients = True
+
+            for ingredient in self.INGREDIENTS[dish]:
+                if self.inventory[ingredient] < 1:
+                    has_ingredients = False
+                if has_ingredients:
+                    avaliable_dishes.add(dish)
+
+        return avaliable_dishes
